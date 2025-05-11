6a990b785d25f543bf28d12e8b3c50b785be63ff
Antoniojrsales <Antoniogomes.junio@gmail.com>
2025-05-11 12:15:27 -0300

    Refatorando Ex003

--- Ex003.py
+++ Ex003.py
@@ -1,7 +1,11 @@
 # Create a program what read two numbers and show the sum between them
 
 # Define a function called 'somar_valores' what performs the sum of two number typed for the user
-def somar_valores():
+def somar_valores(valor1, valor2):
+    resultado = valor1 + valor2
+    return resultado
+
+def main():
     # Requests to the user what totype the first and second value and stores as string
     valor_um = input('Digite o primeiro valor: ') 
     valor_dois = input('Digite o segundo valor: ') 
@@ -10,10 +14,12 @@ def somar_valores():
     if valor_um.isdigit() and valor_dois.isdigit():
         valor_um_int = int(valor_um) # Converting strings for integers
         valor_dois_int = int(valor_dois) 
-        soma = valor_um_int + valor_dois_int # Calculate the sum of the two values
+        soma = somar_valores(valor_um_int, valor_dois_int) # Calculate the sum of the two values
         print(f'A soma entre {valor_um_int} e {valor_dois_int} = {soma}') # displays the result formatted
     else:
         # Case one or both the values not be valid, displays one mesage of error
         print('Valor inválido. Digite um valor válido.')
 
-somar_valores() # call the function for to be executed
+# call the function for to be executed
+if __name__ == '__main__':
+    main()
